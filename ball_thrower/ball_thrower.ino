54204ea560e7ca6b705e47f40c45f0714fc51865
Guilherme Fernandes <57098649+guilfer-dev@users.noreply.github.com>
2024-03-17 14:16:43 -0300

    feat: add end stop switch

--- ball_thrower/ball_thrower.ino
+++ ball_thrower/ball_thrower.ino
@@ -1,5 +1,6 @@
 int VOLTAGE_PIN = A0;
-int COIL_PIN = 2;
+int COIL_PIN = 7;
+int BALL_LISTENER = 2;
 int SECONDS = 1000;
 
 void shoot() {
@@ -11,14 +12,19 @@ void shoot() {
 void setup() {
   Serial.begin(9600);
   pinMode(COIL_PIN, OUTPUT);
+  pinMode(BALL_LISTENER, INPUT_PULLUP);
   Serial.println("Cleaning up capacitors");
   shoot();
 }
 
 void loop() {
-  float voltage = (analogRead(A0) / 1024.0) * 5.2 * 100;
+  float voltage = (analogRead(A0) * 220.0 * 1.14) / 1024.0;
+  // since using pull_up, 0 means has ball, 1 means no ball
+  int ball_in = digitalRead(BALL_LISTENER);
+  Serial.print("voltage: ");
   Serial.println(voltage);
-  if (voltage > 250) {
+  if (voltage > 200 && ball_in == 0) {
+    delay(1.5 * SECONDS);
     Serial.println("Shooting!");
     shoot();
   }
